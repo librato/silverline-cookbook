bd98f7492ca63921e5da4e39e4d85dc9f4b61f06
Mike Heffner <mikeh@fesnel.com>
2011-12-06 13:16:11 -0500

    Update apt cookbook dependency.

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ maintainer        "Librato, Inc."
 maintainer_email  "silverline@librato.com"
 license           "Apache 2.0"
 description       "Installs/Configures Librato Silverline"
-version           "0.0.5"
+version           "0.0.6"
 
-depends 'apt', '~> 1.1.1'
+depends 'apt', '~> 1.2.0'
 
 %w{ubuntu}.each do |os|
   supports os
