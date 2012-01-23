3980d2768dafbef14628f78069228b25610b7fd3
Mike Heffner <mikeh@fesnel.com>
2012-01-23 13:40:33 -0500

    Bump patch version.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Librato, Inc."
 maintainer_email  "silverline@librato.com"
 license           "Apache 2.0"
 description       "Installs/Configures Librato Silverline"
-version           "0.0.6"
+version           "0.0.7"
 
 depends 'apt', '~> 1.2.0'
 
